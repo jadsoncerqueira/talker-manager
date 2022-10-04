ab8a5c157610aab6ab086b40ed27d0d71f6aa89a
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2022-10-04 17:49:09 -0300

    requisito2

--- src/index.js
+++ src/index.js
@@ -8,6 +8,15 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const validationId = (req, res, next) => {
+  const { id } = req.params;
+  if (Number(id) <= 4) {
+    next();
+  } else {
+    res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -16,6 +25,13 @@ app.get('/', (_request, response) => {
 app.get('/talker', async (request, response) => {
   const res = await getData();
   response.status(HTTP_OK_STATUS).json(res);
+});
+
+app.get('/talker/:id', validationId, async (request, response) => {
+  const { id } = request.params;
+  const res = await getData();
+  const find = res.find((el) => el.id === Number(id));
+  response.status(HTTP_OK_STATUS).json(find);
 });
 
 app.listen(PORT, () => {
